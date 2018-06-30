9b47a6940ae12a4bf80db8e4641a5a690124a3dd
lin13177896353@163.com <lin13177896353@163.com>
2018-07-01 04:35:16 +0800

    傻逼eslint

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    hello
-    <router-view/>
+    <m-header></m-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  components: { MHeader }
 }
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>
